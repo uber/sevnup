c156e50cca79a4af9ed5b8df9b705a38a840731d
joseph <joseph@uber.com>
2014-11-25 06:37:42 +0000

    overall 7up functionality

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 var VNodeStore = require('./lib/vnode-store.js');
+//TODO (joseph@): Config.
+var TOTAL_VNODES = 14;
 
 function Sevnup(hashRing, loadVNKeysFromStorage, persistKeyToVNode, persistRemoveKeyFromVNode) {
-    //TODO (joseph@): add hashring attach if exists.
-    //TODO (joseph@): add 'recover' method override, make sure hooks in
-    //    VNodeStore.
+    var allVNodes = [];
+    for (var i=0; i<TOTAL_VNODES; i++) {
+        allVNodes.append(i);
+    }
+    this.allVNodes = allVNodes;
+    this.attachToRing(hashRing);
+    hashRing.on('ringStateChange', this.loadAllKeys);
     this.vnodeStore = new VNodeStore(loadVNKeysFromStorage, persistKeyToVNode, persistRemoveKeyFromVNode);
 }
 
 Sevnup.prototype.loadAllKeys = function loadAllKeys() {
     var self = this;
-    var vnodes = [1,2,3,4,5];
+    var vnodes = self.allVNodes;
     for (var i=0; i < vnodes.length; i++) {
-        if (self.iOwnVnode(vnodes[i])) {
+        if (self.iOwnVNode(vnodes[i])) {
             self.vnodeStore.loadVNodeKeys(vnodes[i]);
         }
     }
+};
+
+/**
+ * Takes a hashRing and subscribes to the correct events to maintain VNode
+ * ownership.
+ * @param {object} hashRing A ringPop implementation of a hashring.
+ */
+Sevnup.prototype.attachToRing = function attachToRing(hashRing) {
+    this.hashRing = hashRing;
+    //TODO (joseph@): Listen to proper event.  Test.
+    hashRing.on('ringStateChange', this.loadAllKeys);
+};
+
+/**
+ * Returns true if this node currently owns vnode.
+ * @param {string} vnodeName The name of the vnode to check.
+ */
+Sevnup.prototype.iOwnVNode = function iOwnVNode(vnodeName) {
+    vnodeName = vnodeName;
+    //TODO (joseph@): Use this.hashRing to check if it owns the node.
 };
 
 module.exports = Sevnup;
