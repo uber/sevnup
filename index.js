97007ef4867ae11d3adce8fc40f95ec9de025a98
joseph <joseph@uber.com>
2014-11-25 04:15:41 +0000

    notes/planning

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function sevnup() {
-    
+var VNodeStore = require('./lib/vnode-store.js');
+
+function Sevnup(hashRing, loadVNodeKeySetsFromDataStore, persistKeyToVNode, persistRemoveKeyFromVNode) {
+    //TODO (joseph@): add hashring attach if exists.
+    //TODO (joseph@): add 'recover' method override, make sure hooks in
+    //    VNodeStore.
+    this.vnodeStore = new VNodeStore(loadVNodeKeySetsFromDataStore, persistKeyToVNode, persistRemoveKeyFromVNode);
 }
 
-module.exports = sevnup;
+Sevnup.prototype.loadAllKeys = function loadAllKeys() {
+    var self = this;
+    self.vnodeStore.loadVNodeKeySets();
+    // TODO (joseph@): for key in vnodeStore, do operation.  if return false,
+    //     delete key.
+    
+};
+
+module.exports = Sevnup;
