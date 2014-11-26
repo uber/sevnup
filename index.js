04495fc740cf3f2cdd78555aac8b4a3b3932b8a4
joseph <joseph@uber.com>
2014-11-26 01:57:53 +0000

    comment, ringpop tie-in

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ var VNodeStore = require('./lib/vnode-store.js');
 //TODO (joseph@): Config.
 var TOTAL_VNODES = 14;
 
+/**
+ * Constructor, takes all optional persistence function overrides but expects none.
+ * @constructor
+ * @param {function} loadVNKeysFromStorage A method that takes a vnode and
+ *     the list of keys it owns, presumably recovered from a datastore.
+ *  @param {function} persistKeyToVNode Given a key and a VNode, this function
+ *      adds the relation to the datastore.
+ *  @param {function} persistRemoveKeyFromVNode The inverse of
+ *      persistKeyToVNode, removes a key relation to a VNode in the store.
+ */
 function Sevnup(loadVNKeysFromStorage, persistKeyToVNode, persistRemoveKeyFromVNode) {
     var allVNodes = [];
     for (var i=0; i<TOTAL_VNODES; i++) {
@@ -11,6 +21,13 @@ function Sevnup(loadVNKeysFromStorage, persistKeyToVNode, persistRemoveKeyFromVN
     this.vnodeStore = new VNodeStore(loadVNKeysFromStorage, persistKeyToVNode, persistRemoveKeyFromVNode);
 }
 
+/**
+ * Checks each VNode to see if the current node owns it, and if it does it
+ * prompts recovery of each key.  For example, it finds that it owns VNode B,
+ * recovers 14 keys that the old owner of VNode B was working on, and prompts
+ * the client via callback to recover each of those keys, leaving that to the
+ * individual client's business logic.
+ */
 Sevnup.prototype.loadAllKeys = function loadAllKeys() {
     var self = this;
     var vnodes = self.allVNodes;
@@ -28,8 +45,7 @@ Sevnup.prototype.loadAllKeys = function loadAllKeys() {
  */
 Sevnup.prototype.attachToRing = function attachToRing(hashRing) {
     this.hashRing = hashRing;
-    //TODO (joseph@): Listen to proper event.  Test.
-    hashRing.on('ringStateChange', this.loadAllKeys);
+    hashRing.on('changed', this.loadAllKeys);
 };
 
 /**
@@ -37,8 +53,8 @@ Sevnup.prototype.attachToRing = function attachToRing(hashRing) {
  * @param {string} vnodeName The name of the vnode to check.
  */
 Sevnup.prototype.iOwnVNode = function iOwnVNode(vnodeName) {
-    vnodeName = vnodeName;
-    //TODO (joseph@): Use this.hashRing to check if it owns the node.
+    var node = self.ringpop.lookup(vnodeName);
+    return self.ringpop.whoami() === node;
 };
 
 module.exports = Sevnup;
