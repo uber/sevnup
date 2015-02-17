98d1bb53f16d6447344336f0f9f74632d8bd8264
Iain Proctor <iain@uber.com>
2015-02-17 22:16:08 +0000

    use has

--- cache_store.js
+++ cache_store.js
@@ -1,19 +1,21 @@
 module.exports = CacheStore;
 
+var _ = require('lodash');
+
 function CacheStore(store) {
     this.store = store;
     // Only stores sets that have been loaded
     // Contains map of key -> bool, where bool isn't important
-    this.cache = {};
+    this.cache = Object.create(null);
 }
 
 CacheStore.prototype.addKey = function addKey(vnode, key, done) {
-    var set = this.cache[vnode];
-    if (!set) {
+    if (!_.has(this.cache, vnode)) {
         // Hasn't been fully loaded into cache
         return this.store.addKey(vnode, key, done);
     }
-    if (set.hasOwnProperty(key)) {
+    var set = this.cache[vnode];
+    if (_.has(set, key)) {
         return done();
     }
     this.store.addKey(vnode, key, function(err) {
@@ -25,12 +27,12 @@ CacheStore.prototype.addKey = function addKey(vnode, key, done) {
 };
 
 CacheStore.prototype.removeKey = function removeKey(vnode, key, done) {
-    var set = this.cache[vnode];
     // Set is loaded, key is not present.
     // TODO if you want to get really fancy, store a tombstone on remove and check that too.
-    if (set && !set.hasOwnProperty(key)) {
+    if (_.has(this.cache, vnode) && !_.has(this.cache[vnode], key)) {
         return done();
     }
+    var set = this.cache[vnode];
     this.store.removeKey(vnode, key, function(err) {
         if (!err && set) {
             delete set[key];
@@ -41,12 +43,12 @@ CacheStore.prototype.removeKey = function removeKey(vnode, key, done) {
 
 CacheStore.prototype.loadKeys = function loadKeys(vnode, done) {
     var self = this;
-    if (this.cache[vnode]) {
+    if (_.has(this.cache, vnode)) {
         return done(null, Object.keys(this.cache[vnode]));
     }
     this.store.loadKeys(vnode, function(err, keys) {
         if (!err) {
-            var set = self.cache[vnode] = {};
+            var set = self.cache[vnode] = Object.create(null);
             keys.forEach(function(key) {
                 set[key] = true;
             });
